6e54576d3e3c81925e4c6a739982318852451e5f
Sourabh Sinha <sourabh.sinha@navihealth.com>
2020-12-11 00:04:59 +0530

    Made changes to trigger blue deployment

---
+++ src/main/java/com/k8team2020/demoProj/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.k8team2020.demoProj.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    
+    @GetMapping({"/", "index.html"})
+    public String index(){
+        return "Micro app service is ready for you";
+    }
+
+}
---
+++ src/main/java/com/k8team2020/demoProj/models/Version.java
@@ -0,0 +1,20 @@
+package com.k8team2020.demoProj.models;
+
+public class Version {
+
+    private String version;
+
+    public Version(String version) {
+        this.version = version;
+    }
+    public Version() {
+    }
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+    
+}
