8fa66da004943194cb29bc8088aef6ce2a9aac7c
Harsit Mohapatra <harsittheking001@gmail.com>
2020-12-14 13:00:05 +0530

    Update Version.java
    Changes to Model - VC
    #34

--- src/main/java/com/k8team2020/demoProj/models/Version.java
+++ src/main/java/com/k8team2020/demoProj/models/Version.java
@@ -3,18 +3,30 @@ package com.k8team2020.demoProj.models;
 public class Version {
 
     private String version;
-
-    public Version(String version) {
-        this.version = version;
-    }
+    private String deploymentName;
+    
     public Version() {
     }
+    
+    public Version(String version, String deploymentName) {
+        this.version = version;
+        this.deploymentName = deploymentName;
+    }
+    
     public String getVersion() {
         return version;
+    }
+    
+    public String getDeploymentName(){
+        return deploymentName;
     }
 
     public void setVersion(String version) {
         this.version = version;
+    }
+    
+    public void setDeploymentName(String deploymentName){
+        this.deploymentName = deploymentName;
     }
     
 }
